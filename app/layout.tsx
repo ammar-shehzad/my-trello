cdb65638e53075098e877479fcd3f14053844823
ammar <saifullahkha09090@gmail.com>
2026-04-21 19:53:43 +0500

    saving my work

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,45 +17,10 @@ export default function RootLayout({
       lang="en"
   
     >
-<>
+<head>
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/7.0.1/css/all.min.css" />
 
-<nav className="navbar navbar-expand-lg bg-body-tertiary">
-  <div className="container-fluid">
-    <a className="navbar-brand" href="#">My Trello</a>
-    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-      <span className="navbar-toggler-icon"></span>
-    </button>
-    <div className="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-        <li className="nav-item">
-          <a className="nav-link active" aria-current="page" href="#">Home</a>
-        </li>
-        <li className="nav-item">
-          <a className="nav-link" href="#">Link</a>
-        </li>
-        <li className="nav-item dropdown">
-          <a className="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-            Dropdown
-          </a>
-          <ul className="dropdown-menu">
-            <li><a className="dropdown-item" href="#">Action</a></li>
-            <li><a className="dropdown-item" href="#">Another action</a></li>
-            <li><hr className="dropdown-divider"/></li>
-            <li><a className="dropdown-item" href="#">Something else here</a></li>
-          </ul>
-        </li>
-        <li className="nav-item">
-          <a className="nav-link disabled" aria-disabled="true">Disabled</a>
-        </li>
-      </ul>
-      <form className="d-flex" role="search">
-        <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
-        <button className="btn btn-outline-success" type="submit">Search</button>
-      </form>
-    </div>
-  </div>
-</nav>
-</>
+</head>
       <body className="min-h-full flex flex-col">{children}</body>
 
     </html>
--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 import Image from "next/image";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 export default function Home() {
   let [task, setTask] = useState<{ newTask: any; category: any }>();
@@ -13,8 +13,9 @@ export default function Home() {
   let [cards,setCards]=useState<{name:string,color:any}[]>([{name:"today",color:"#533F04"},{name:"this week",color:"#19573D"},{name:"later",color:"#101204"}])
 let [err,setErr]=useState("")
 let [err2,setErr2]=useState("")
-
-
+let [isCollapse,setIsCollapse]=useState<boolean>(true)
+let[isActiveId,setIsActiveId]=useState<string|null>(null)
+let [editTask,setEditTask]=useState<{task:string,period:string,index:any}>({task:"",period:"",index:""})
 
   let TodayTask = todos.filter((todos) => todos.period == "today");
   let LaterTask = todos.filter((todos) => todos.period == "later");
@@ -96,8 +97,10 @@ setTask2((prev:any)=>{
 
 }
 
-const handleTask2Submit=(cardName:any)=>{
+const handleTask2Submit=(cardName:any,id:any)=>{
 
+setIsActiveId(id)
+setIsCollapse(false)
 
 if(task2?.newTask!=""){
 setTodos((prev:any)=>{
@@ -124,20 +127,49 @@ setTask2((prev:any)=>{
   }
 })
 
+setIsActiveId(null)
+
 }else{
   setErr2("Enter Your Task")
+
+}
+
+
+
+}
+
+const handleTaskEdit=(index:any,period:any)=>{
+
+if(editTask?.task!="")
+setTodos((prev)=>{
+return prev.map((t,i)=>{
+   return i==index && t.period==period?
+    {...t,task:editTask?.task??t.task}:
+    t
+  })
+
+
+})
+}
+
+const handleEditTaskChange=(e:any)=>{
+let {name,value}=e.target
+
+setEditTask({
+  ...editTask,
+  [name]:value
+})
+
 }
 
 
 
-}
-
-  console.log(cards);
+  console.log(editTask.index);
 
   return (
     <>
       {/* Todo Task Form Starts */}
-      <div className="container my-5">
+      <div className="container">
         <div className="row">
           <div className="col-lg-12">
             <div className="form-group d-flex ">
@@ -205,15 +237,15 @@ setTask2((prev:any)=>{
 
 {/* ==========================Starting Changes============ */}
   {/* Todo New Cards Starts */}
-      <div className="container my-5">
-        <div className="row rounded  p-5 flex-row flex-nowrap " style={{overflowX:"scroll",backgroundColor:"#F54927"}}>
+      <div className="container ">
+        <div className="row rounded  p-1 flex-row flex-nowrap " style={{overflowX:"scroll",backgroundColor:"#DBDBDB"}}>
           {cards.map((c,i)=>{
             return(
               <>
           <div key={i} className="col-lg-4 my-3">
             <div
               className="card rounded "
-              style={{ backgroundColor: c.color }}
+              style={{ backgroundColor: "#101204" }}
             >
               <div className="card-body text-light" style={{height:"50vh",overflowY:"scroll"}}>
                 <h4 className="card-title">{c.name}</h4>
@@ -224,7 +256,14 @@ setTask2((prev:any)=>{
                       <>
                         <li key={i} className="list-group-item rounded d-flex justify-content-between my-1">
                           {t.task}
+                          
                           <div className="form-check">
+                              <span onClick={()=>{
+                                setEditTask({task:t.task,period:t.period,index:i})
+                              }} data-bs-target="#editmodel" data-bs-toggle="modal"><i className="fa-solid fa-pen mx-1" style={{color: "rgb(148, 166, 17)"}}></i></span>
+                          <span>
+                            <i className="fa-solid fa-delete-left" style={{color: "rgb(148, 166, 17)"}}></i>
+                          </span>
                             <input
                               className="form-check-input"
                               type="checkbox"
@@ -243,9 +282,15 @@ setTask2((prev:any)=>{
 
 
               </div>
-              <div className="card-footer d-flex flex-row">
-                <input placeholder={err2} name="newTask" value={task2?.newTask}  type="text" className="form-control collapse mx-1" id={i.toString()} onChange={handleTask2Change} />
-                <button className="btn btn-light" data-bs-target={`#${i}`} data-bs-toggle="collapse" onClick={()=>handleTask2Submit(c.name)} >+</button>
+              <div className="card-footer  d-flex">
+                <input placeholder={err2} name="newTask" value={task2?.newTask}  type="text" className="form-control  mx-1" id={i.toString()} onChange={handleTask2Change}   style={{ display: isActiveId === i.toString() ? "block" : "none" }}
+ />
+                <button className="btn btn-light"  onClick={()=>{
+                  setIsActiveId(i.toString())
+handleTask2Submit(c.name,i.toString())
+
+
+                }} >Add Task</button>
               </div>
 
             </div>
@@ -259,6 +304,37 @@ setTask2((prev:any)=>{
       </div>
 
       {/* Todo New Cards Ends */}
+
+
+
+
+{/*================= model box=============================== */}
+
+
+<div className="modal"  role="dialog" id="editmodel">
+  <div className="modal-dialog" role="document">
+    <div className="modal-content">
+      <div className="modal-header">
+        <h5 className="modal-title">Edit Task</h5>
+        {/* <button type="button" className="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button> */}
+      </div>
+      <div className="modal-body">
+
+
+  <input type="text" name="task" className="form-control my-2" value={editTask?.task} onChange={handleEditTaskChange}/>
+  <input type="text" name="period" className="form-control my-2" value={editTask?.period} onChange={handleEditTaskChange} />
+   <input type="text" name="index" className="form-control my-2" value={editTask?.index} onChange={handleEditTaskChange}/>
+
+  <button className="btn btn-primary" name="" onClick={()=>{handleTaskEdit(editTask?.index,editTask.period)}}>Save</button>
+
+      </div>
+      
+    </div>
+  </div>
+</div>
+
 
 
     </>
